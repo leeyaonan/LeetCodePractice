31b3abf2a3b75a1ff49878cf5c22c47c33159fd9
leeyaonan <leeyaonan@outlook.com>
2020-06-15 17:28:48 +0800

    回文数

--- leetcode/editor/cn/[9]回文数.java
+++ leetcode/editor/cn/[9]回文数.java
@@ -29,7 +29,8 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean isPalindrome(int x) {
-        return this.isPalindrome1(x);
+//        return this.isPalindrome1(x);
+        return this.isPalindrome2(x);
     }
 
     public boolean isPalindrome1(int x) {
@@ -50,6 +51,54 @@ class Solution {
             newStr = newStr + String.valueOf(chars[i]);
         }
         return intStr.equals(newStr);
+    }
+
+    public boolean isPalindrome2(int x) {
+        /**
+         * 方案二：
+         *      只比较数字的一半，当数字位数为奇数（n）时，只要比较后n%2位翻转与前n%2位一致不一致即可
+         *      eg：121，只需要比较1和1，12321只需要比较21翻转是否=12
+         *
+         *      并且考虑一些边界条件：
+         *      1. 所有的负数肯定不是回文数
+         *      2. 除0以外最后一位为0的肯定不是回文数（因为除了0没有第一位为0的整数）
+         * 提交结果：
+         * 解答成功:
+         * 			执行耗时:10 ms,击败了66.25% 的Java用户
+         * 			内存消耗:39 MB,击败了5.14% 的Java用户
+         */
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        // revertNumber记录原始数字一半（向下取整）的的反转后的数字
+        int revertNumber = 0;
+
+        while (x > revertNumber) {
+            revertNumber = revertNumber * 10 + x % 10;
+            x /= 10;
+        }
+
+        // 偶数的revertNum =
+        return x == revertNumber || x == revertNumber / 10;
+
+        /*
+        eg1：
+            x = 121
+            step1: revertNumber = 0,x = 121
+            step2: x > revertNumber -> revertNumber = 0 * 10 + 121 % 10 = 1, x = 121 / 10 = 12
+            step3: x > revertNumber -> revertNumber = 1 * 10 + 12 % 10 = 12, x = 12 / 10 = 1
+            step4: x < revertNumber -> revertNumber = 12, x = 1
+            step5: x == revertNumber / 10 = 1 -> true
+
+        eg2：
+            x = 1221
+            step1: revertNumber = 0,x = 1221
+            step2: x > revertNumber -> revertNumber = 0 * 10 + 1221 % 10 = 1, x = 1221 / 10 = 122
+            step3: x > revertNumber -> revertNumber = 1 * 10 + 122 % 10 = 12, x = 122 / 10 = 12
+            step4: x == revertNumber -> revertNumber = 12, x = 12
+            step5: x == revertNumber = 12 -> true
+         */
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
