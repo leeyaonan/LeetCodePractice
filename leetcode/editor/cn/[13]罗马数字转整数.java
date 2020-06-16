8fc91b96fa9f78872b103cbd1af2d9b7e7774f8f
leeyaonan <leeyaonan@outlook.com>
2020-06-16 17:11:08 +0800

    plan2

--- leetcode/editor/cn/[13]罗马数字转整数.java
+++ leetcode/editor/cn/[13]罗马数字转整数.java
@@ -56,7 +56,8 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int romanToInt(String s) {
-        return this.romanToInt1(s);
+//        return this.romanToInt1(s);
+        return this.romanToInt2(s);
     }
 
     public static final String I = "I";
@@ -118,6 +119,43 @@ class Solution {
             return 0;
         }
         return ROMANS_2_INT.get(roman);
+    }
+
+    public int romanToInt2(String s) {
+        /**
+         * 方案二：
+         *      把一个小值放在大值的左边，就是做减法，否则为加法
+         * 执行结果：
+         * 解答成功:
+         * 			执行耗时:4 ms,击败了99.98% 的Java用户
+         * 			内存消耗:40 MB,击败了5.56% 的Java用户
+         */
+        int sum = 0;
+        int preNum = getValue(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            int num = getValue(s.charAt(i));
+            if (preNum < num) {
+                sum -= preNum;
+            } else {
+                sum += preNum;
+            }
+            preNum = num;
+        }
+        sum += preNum;
+        return sum;
+    }
+
+    public int getValue(char ch) {
+        switch (ch) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
